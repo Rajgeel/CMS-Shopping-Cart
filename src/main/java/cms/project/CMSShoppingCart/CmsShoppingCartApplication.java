09559e4e50ca4604f348fa075af64fb0d2e0bded
Rohit Kumar <rohitrajgeel000@gmail.com>
2020-12-30 22:02:26 +0530

    initial commit

---
+++ src/main/java/cms/project/CMSShoppingCart/CmsShoppingCartApplication.java
@@ -0,0 +1,13 @@
+package cms.project.CMSShoppingCart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsShoppingCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsShoppingCartApplication.class, args);
+	}
+
+}
