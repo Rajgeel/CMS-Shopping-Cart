3d569a459eccb21265b5850b7b077466fef66bcb
Rohit Kumar <rohitrajgeel000@gmail.com>
2021-01-02 17:38:06 +0530

    added admin section start

---
+++ src/main/java/cms/project/CMSShoppingCart/controllers/AdminPagesController.java
@@ -0,0 +1,16 @@
+package cms.project.CMSShoppingCart.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/pages")
+public class AdminPagesController {
+
+    @GetMapping
+    public String index() {
+        return "admin/pages/index";
+    }
+    
+}
