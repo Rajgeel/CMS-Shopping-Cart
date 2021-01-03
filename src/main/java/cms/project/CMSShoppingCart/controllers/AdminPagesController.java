3c0d3ab6f3d3360526b8a2c52d781ab6e34465e8
Rohit Kumar <rohitrajgeel000@gmail.com>
2021-01-04 00:44:32 +0530

    modified admin section

--- src/main/java/cms/project/CMSShoppingCart/controllers/AdminPagesController.java
+++ src/main/java/cms/project/CMSShoppingCart/controllers/AdminPagesController.java
@@ -1,15 +1,32 @@
 package cms.project.CMSShoppingCart.controllers;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import cms.project.CMSShoppingCart.models.PageRepository;
+import cms.project.CMSShoppingCart.models.entity.Page;
 
 @Controller
 @RequestMapping("/admin/pages")
 public class AdminPagesController {
 
+    private PageRepository pageRepository;
+
+    public AdminPagesController(PageRepository pageRepository) {
+        this.pageRepository = pageRepository;
+    }
+
     @GetMapping
-    public String index() {
+    public String index(Model model) {
+
+        List<Page> pages = pageRepository.findAll();
+
+        model.addAttribute("pages", pages);
+
         return "admin/pages/index";
     }
     
---
+++ src/main/java/cms/project/CMSShoppingCart/models/PageRepository.java
@@ -0,0 +1,10 @@
+package cms.project.CMSShoppingCart.models;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import cms.project.CMSShoppingCart.models.entity.Page;
+
+public interface PageRepository extends JpaRepository<Page, Integer>{
+
+}
