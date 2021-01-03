b09a949740bbef7880696c2081ce7a70a5dbff19
Rohit Kumar <rohitrajgeel000@gmail.com>
2021-01-04 01:51:53 +0530

    added GET add page

--- src/main/java/cms/project/CMSShoppingCart/controllers/AdminPagesController.java
+++ src/main/java/cms/project/CMSShoppingCart/controllers/AdminPagesController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import cms.project.CMSShoppingCart.models.PageRepository;
@@ -28,6 +29,14 @@ public class AdminPagesController {
         model.addAttribute("pages", pages);
 
         return "admin/pages/index";
+    }
+
+    @GetMapping("/add")
+    public String add(@ModelAttribute Page page) {
+
+        //model.addAttribute("page", new Page());
+
+        return "admin/pages/add";
     }
     
 }
