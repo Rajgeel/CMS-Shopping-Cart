b34d639e92cd06333e8dc7fb6c3d0804ba3a93fa
Rohit Kumar <rohitrajgeel000@gmail.com>
2021-01-21 12:49:29 +0530

    edited Page edit page

--- src/main/java/cms/project/CMSShoppingCart/controllers/AdminPagesController.java
+++ src/main/java/cms/project/CMSShoppingCart/controllers/AdminPagesController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -79,4 +80,50 @@ public class AdminPagesController {
         return "redirect:/admin/pages/add";
     }
     
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable int id, Model model) {
+
+        Page page = pageRepository.getOne(id);
+
+        model.addAttribute("page", page);
+
+        return "admin/pages/edit";
+
+    }
+
+
+    @PostMapping("/edit")
+    public String edit(@Valid Page page, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+        Page pageCurrent = pageRepository.getOne(page.getId());
+
+        if(bindingResult.hasErrors()) {
+            model.addAttribute("pageTitle", pageCurrent.getTitle());
+            return "admin/pages/edit";
+        }
+
+        redirectAttributes.addFlashAttribute("message", "Page edited");
+        redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+
+        String slug = page.getSlug() == "" ? page.getTitle().toLowerCase().replace(" ", "-") : page.getSlug().toLowerCase().replace(" ", "-");
+
+        Page slugExists = pageRepository.findBySlugAndIdNot(slug, page.getId());
+
+        if(slugExists != null) {
+
+            redirectAttributes.addFlashAttribute("message", "Slug exists, choose another");
+            redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+            redirectAttributes.addFlashAttribute("page", page);
+
+
+        }
+        else {
+            page.setSlug(slug);
+
+            pageRepository.save(page);
+        }
+
+
+        return "redirect:/admin/pages/edit/" + page.getId();
+    }
 }
--- src/main/java/cms/project/CMSShoppingCart/models/PageRepository.java
+++ src/main/java/cms/project/CMSShoppingCart/models/PageRepository.java
@@ -1,12 +1,17 @@
 package cms.project.CMSShoppingCart.models;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+// import org.springframework.data.jpa.repository.Query;
 
 import cms.project.CMSShoppingCart.models.entity.Page;
 
 public interface PageRepository extends JpaRepository<Page, Integer>{
 
     Page findBySlug(String slug);
+
+    // @Query("SELECT p FROM Page p WHERE p.id != :id and p.slug = :slug")
+    // Page findBySlug(int id, String slug);
+
+    Page findBySlugAndIdNot(String slug, int id);
 
 }
