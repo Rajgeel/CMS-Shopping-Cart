3f3013e212dcea0daaf6ae4bf8ca5745ebf6b8bd
Rohit Kumar <rohitrajgeel000@gmail.com>
2021-01-22 13:20:03 +0530

    added GET edit category

--- src/main/java/cms/project/CMSShoppingCart/controllers/AdminCategoriesController.java
+++ src/main/java/cms/project/CMSShoppingCart/controllers/AdminCategoriesController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -76,5 +77,16 @@ public class AdminCategoriesController {
 
 
         return "redirect:/admin/categories/add";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable int id, Model model) {
+
+        Category category = categoryRepository.getOne(id);
+
+        model.addAttribute("category", category);
+
+        return "admin/categories/edit";
+
     }
 }
