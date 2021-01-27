29e82c08865641cf85f40e4e9b01d21125903bae
Rohit Kumar <rohitrajgeel000@gmail.com>
2021-01-28 00:43:49 +0530

    added POST edit product

--- src/main/java/cms/project/CMSShoppingCart/controllers/AdminProductsController.java
+++ src/main/java/cms/project/CMSShoppingCart/controllers/AdminProductsController.java
@@ -130,4 +130,77 @@ public class AdminProductsController {
 
     }
 
+    
+    @PostMapping("/edit")
+    public String edit(@Valid Product product, BindingResult bindingResult,MultipartFile file, RedirectAttributes redirectAttributes, Model model) throws IOException {
+
+        Product currentProduct = productRepository.getOne(product.getId());
+
+        List<Category> categories = CategoryRepository.findAll();
+
+        if(bindingResult.hasErrors()) {
+            model.addAttribute("productName", currentProduct.getName());
+            model.addAttribute("categories", categories);
+            return "admin/products/edit";
+        }
+
+        boolean fileOK = false;
+        byte[] bytes = file.getBytes();
+        String filename = file.getOriginalFilename();
+        Path path = Paths.get("src/main/resources/static/media/" + filename);
+
+        if(!file.isEmpty()) {
+            if(filename.endsWith("jpg") || filename.endsWith("png")) {
+                fileOK = true;
+            }
+        }
+        else {
+            fileOK = true;
+        }
+        
+
+        redirectAttributes.addFlashAttribute("message", "Product edited");
+        redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+
+        String slug = product.getName().toLowerCase().replace(" ", "-");
+
+        Product productExists = productRepository.findBySlugAndIdNot(slug, product.getId());
+
+
+        if(! fileOK) {
+
+            redirectAttributes.addFlashAttribute("message", "Image must be a jpg or a png exists");
+            redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+            redirectAttributes.addFlashAttribute("product", product);
+        }
+
+        else if(productExists != null) {
+
+            redirectAttributes.addFlashAttribute("message", "Product exists, choose another");
+            redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+            redirectAttributes.addFlashAttribute("product", product);
+
+
+        }
+        else {
+            product.setSlug(slug);
+
+            if(!file.isEmpty()){
+                Path path2 = Paths.get("src/main/resources/static/media/" + currentProduct.getImage());
+                Files.delete(path2);
+                product.setImage(filename);
+                Files.write(path, bytes);
+            }
+            else {
+                product.setImage(currentProduct.getImage());
+            }
+            
+            productRepository.save(product);
+
+        }
+
+
+        return "redirect:/admin/products/edit/" + product.getId();
+    }
+
 }
--- src/main/java/cms/project/CMSShoppingCart/models/ProductRepository.java
+++ src/main/java/cms/project/CMSShoppingCart/models/ProductRepository.java
@@ -7,5 +7,7 @@ import cms.project.CMSShoppingCart.models.entity.Product;
 public interface ProductRepository extends JpaRepository<Product, Integer>{
 
 	Product findBySlug(String slug);
+
+	Product findBySlugAndIdNot(String slug, int id);
     
 }
