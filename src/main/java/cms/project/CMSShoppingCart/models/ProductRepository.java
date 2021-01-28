84dfc5d8ed9dc3a04c10b5b771fef6618514d280
Rohit Kumar <rohitrajgeel000@gmail.com>
2021-01-28 10:27:41 +0530

    added admin product pagination

--- src/main/java/cms/project/CMSShoppingCart/models/ProductRepository.java
+++ src/main/java/cms/project/CMSShoppingCart/models/ProductRepository.java
@@ -1,5 +1,7 @@
 package cms.project.CMSShoppingCart.models;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import cms.project.CMSShoppingCart.models.entity.Product;
@@ -9,5 +11,7 @@ public interface ProductRepository extends JpaRepository<Product, Integer>{
 	Product findBySlug(String slug);
 
 	Product findBySlugAndIdNot(String slug, int id);
+
+	Page<Product> finadAll(Pageable pagable);
     
 }
